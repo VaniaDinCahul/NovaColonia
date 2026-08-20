f974e56d82390fca174614bd0626f47ee57714c1
VaniaDinCahul <caliniacovlev@gmail.com>
2026-08-20 17:22:13 +0300

    init

---
+++ core/src/main/java/com/github/vaniadincahul/novacolonia/Core.java
@@ -0,0 +1,7 @@
+package com.github.vaniadincahul.novacolonia;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Core extends ApplicationAdapter {
+}
